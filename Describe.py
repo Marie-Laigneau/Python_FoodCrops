d5a50b60e84388b7ad27b528b15c57b18622f672
LIZIAN <lizian@localhost>
2019-11-24 23:31:06 +0100

    added new file:   Describe.py

---
+++ Describe.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Describe.py
+Abstract interface for describe method
+@author: 
+"""
+
+from ABC import ABCMeta, abstractmethod
+
+class Descriptor:
+    @abstractmethod
+    def describe(self):
+        desc = self.__class__.__name__
+        return desc
